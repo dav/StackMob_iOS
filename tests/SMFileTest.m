4b0f1291435f6324b9f6be745498eaeec26495f0
Aaron Schlesinger <arschles@gmail.com>
2012-01-24 19:32:15 -0800

    fixing file upload the multi line strings used in SMFile's JSON method were causing leading whitespace in the body of the file upload. said whitespace is causing payloads that look like this: http://hastebin.com/raw/malunaxibe, which was causing an error on the server. This commit changes those multiline strings to remove the leading whitespace.
    Also, on another note, NSData+Base64 and NSData+JSON weren't included in the tests target so tests were failing to run. This commit fixes that as well

--- tests/SMFileTest.m
+++ tests/SMFileTest.m
@@ -12,12 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <UIKit/UIKit.h>
-#import "NSData+Base64.h"
 #import "SMFileTest.h"
-#import "SMFile.h"
-
-//#import "application_headers" as required
 
 @implementation SMFileTest
 - (void)setUp {
@@ -35,12 +30,12 @@
     SMFile *file =  [[SMFile alloc] initWithFileName:fName data:data contentType:contentType];
     NSString *encodedString = [data base64EncodedString];
     
-    NSString *expectedResult = [NSString stringWithFormat:@"Content-Type: %@\n\
-                                Content-Disposition: attachment; filename=%@\n\
-                                Content-Transfer-Encoding: %@\n\n\
-                                %@", data, fName, contentType, encodedString];
-    
-    bool result = [expectedResult isEqualToString:[file JSON]];
+    NSString *expectedResult = [NSString stringWithFormat:@"Content-Type: %@\n"
+                                "Content-Disposition: attachment; filename=%@\n"
+                                "Content-Transfer-Encoding: base64\n\n"
+                                "%@", contentType, fName, encodedString];
+    NSString * actualResult = [file JSON];
+    bool result = [expectedResult isEqualToString:actualResult];
     STAssertTrue(result, @"JSON serialization failed");
     [file release];
 }
