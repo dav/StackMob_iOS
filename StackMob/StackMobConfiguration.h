6bdfe522d31a81625fcae01345242a024ae97033
Douglas Rapp <drapp@stackmob.com>
2012-02-21 14:11:51 -0800

    Restrict logging to debug mode We should only log for debugging purposes. It looks like there was some plumbing laid out for this, but it was never implemented. I'll document how to turn on logging.

--- StackMob/StackMobConfiguration.h
+++ StackMob/StackMobConfiguration.h
@@ -12,14 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if DEBUG
-    #define SMLog(format, ...) {NSLog(format, ##__VA_ARGS__);}
-    #define StackMobDebug(format, ...) {NSLog([[NSString stringWithFormat:@"[%s, %@, %d] ", __PRETTY_FUNCTION__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__] stringByAppendingFormat:format, ##__VA_ARGS__]);}
-#else
-    #define SMLog(format, ...) {NSLog(format, ##__VA_ARGS__);}
-    #define StackMobDebug(format, ...) {NSLog([[NSString stringWithFormat:@"[%s, %@, %d] ", __PRETTY_FUNCTION__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__] stringByAppendingFormat:format, ##__VA_ARGS__]);}
-#endif
-
 
 #define STACKMOB_PUBLIC_KEY         @"YOUR_PUBLIC_KEY"
 #define STACKMOB_PRIVATE_KEY        @"YOUR_PRIVATE_KEY"
