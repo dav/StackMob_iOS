ba562e37754c458383b446042d82244e05f884b6
Dav Yaginuma <dav@akuaku.org>
2013-01-08 10:52:10 -0800

    DIY: TODO, this must be a leak

--- StackMob/StackMob.m
+++ StackMob/StackMob.m
@@ -76,7 +76,7 @@ static SMEnvironment environment;
         _sharedManager.requests = [NSMutableArray array];
         _sharedManager.callbacks = [NSMutableArray array];
         _sharedManager.queueLock = [[[NSLock alloc] init] autorelease];
-        _sharedManager.cookieStore = [[[StackMobCookieStore alloc] initWithSession:_sharedManager.session] retain];
+      _sharedManager.cookieStore = [[[StackMobCookieStore alloc] initWithSession:_sharedManager.session] retain]; // this is surely a leak?
     }
     return _sharedManager;
 }
